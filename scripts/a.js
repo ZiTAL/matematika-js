5f6c2f2048032a148e2912be15eda7fe00b23bf1
zital <zital@riseup.net>
2020-06-24 19:16:27 +0200

    script

--- scripts/a.js
+++ scripts/a.js
@@ -1,9 +1,9 @@
 const RandomNearNumbers = require("../src/lib/random-near-numbers.js");
-
-let operators = "+-/*".match(/[^\s]{1}/g);
+let operators = "+-*/".match(/[^\s]{1}/g);
+let exercises = [];
 for(let i=0; i<10; i++)
 {
-    let one, two, operator, result;
+    let one, two, operator, result, obj = {}, valid_result = false;
     do
     {
         one = RandomNearNumbers(
@@ -24,15 +24,54 @@ for(let i=0; i<10; i++)
             min: 0,
             max: 10
         });
+
         result = new Function("return "+one + operator +two)();
+
+        valid_result = isValidResult(result);
+        if(valid_result)
+        {
+            let a = RandomNearNumbers(
+            {
+                value: result,
+                amount: 2
+            });        
+    
+            obj =
+            {
+                one: one,
+                operator: operator,
+                two: two,
+                result: a
+            };
+        }
     }
-    while(result===Infinity || result===-Infinity || result<0 || result % 1 !==0 || isNaN(result))
+    while(exerciseExist(obj, exercises) || !valid_result)
 
+    exercises.push(obj);
+}
 
-    var a = RandomNearNumbers(
+exercises.map(function(exercise)
+{
+//  console.log(exercise.result);
+    console.log(exercise.one+" "+exercise.operator+" "+exercise.two+" = "+exercise.result.result);
+});
+
+function exerciseExist(obj, exercises)
+{
+    let l = exercises.length;
+    for(let i=0; i<l; i++)
     {
-        value: result,
-        amount: 2
-    });
-    console.log(one+" "+operator+" "+two+" = ",a);
+        if((obj.operator==='+' || obj.operator==='*') && exercises[i].one===obj.two && exercises[i].two===obj.one && exercises[i].operator===obj.operator)
+            return true;
+        if(exercises[i].one===obj.one && exercises[i].two===obj.two && exercises[i].operator===obj.operator)
+            return true;
+    }
+    return false;
+}
+
+function isValidResult(result)
+{
+    if(result===Infinity || result===-Infinity || result<0 || result % 1 !==0 || isNaN(result))
+        return false;
+    return true;
 }
